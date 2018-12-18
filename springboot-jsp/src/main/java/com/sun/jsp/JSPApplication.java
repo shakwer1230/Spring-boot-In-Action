399780ae471c233a1158ff196d91b24417f3bba4
lvxiaoke <lvxiaoke@fang.com>
2018-12-18 15:14:30 +0800

    1111

---
+++ springboot-jsp/src/main/java/com/sun/jsp/JSPApplication.java
@@ -0,0 +1,17 @@
+package com.sun.jsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @
+ * @Author:lvxiaoke
+ * @Date: 2018/12/18
+ * @
+ */
+@SpringBootApplication
+public class JSPApplication {
+    public static void main(String[]args){
+        SpringApplication.run(JSPApplication.class,args);
+    }
+}
